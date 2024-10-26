7b34c8a1eff26c848b1ec60cee0830d1399ffe70
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-10-26 15:05:47 +0200

    added methods constructor getter and setter for password also a base64 hash

--- password.java
+++ password.java
@@ -2,29 +2,48 @@ import java.util.Scanner;
 
 
 
-
 //  creating the password class with all the method it needs to be able to let the user create safe passwords and then also safe them inside 
 public class password {
 
     private boolean wantsToCreatePassword = false;
 
+
+    private String password;
+
+
+    
+
     public static void main(String[] args) {
        
         
     }
-    private String password;
+    
 
+    // Constructor where we initialize
     public password(String password) {
         this.password = password;
     }
-
+    // getter
     public String getPassword() {
         return password;
     }
 
+    //setter
     public void setPassword(String password) {
         this.password = password;
     }
+
+    //method to crypt Base64
+    public static String encodeToBase64(String message) {
+        return Base64.getEncoder().encodeToString(message.getBytes());
+      }
+      
+      public static String decodeFromBase64(String encodedMessage) {
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedMessage);
+        String decodedString = new String(decodedBytes);
+        return decodedString;
+      }
+   
 
 
     
@@ -35,12 +54,25 @@ public class password {
         System.out.println("Create Random Crypted Password ?");
         String userResponse = UserInput.nextLine();
         this.wantsToCreatePassword = userResponse.equalsIgnoreCase("yes");
-        // condition if the boolean is true
-        if(wantsToCreatePassword == true){
-            //hash 
-           
-          
 
+
+        
+
+
+
+        // condition if the boolean is true
+        if(userResponse.contains("yes")){
+            //hash 
+            wantsToCreatePassword = true;
+           
+
+
+                     
+        }
+
+        if(wantsToCreatePassword == true) {
+
+            encodeToBase64(password);
 
         }
 
